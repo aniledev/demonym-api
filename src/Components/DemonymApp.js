1cb4c70a9583c97a75f0b8e3a0cd81d30b19c7ec
aniledev <elinamcgill@gmail.com>
2021-01-05 23:08:05 -0500

    FEAT: add methods to handle failure of the promise

--- src/Components/DemonymApp.js
+++ src/Components/DemonymApp.js
@@ -20,13 +20,26 @@ export default class DemonymApp extends Component {
   }
 
   componentDidMount() {
-    fetch("https://country.register.gov.uk/records.json?page-size=5000")
+    fetch("https://country.register.gov.uk/recordsWRONG.json?page-size=5000")
+      .then((response) => {
+        // check if response is ok
+        console.log("About to check for errors");
+        if (!response.ok) {
+          console.log("An error did occur, let's throw an error.");
+          throw new Error("Something went wrong"); // throw an error
+        }
+        return response; // ok, so just continue
+      })
       .then((response) => response.json())
       .then((data) => {
         const countries = Object.keys(data).map((key) => data[key].item[0]);
-        console.log(countries);
         this.setState({
           countries,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error: err.message,
         });
       });
   }
@@ -41,8 +54,15 @@ export default class DemonymApp extends Component {
       <div className="demonym_app__placeholder">Select a country above</div>
     );
 
+    const error = this.state.error ? (
+      <div className="demonym_app__error">{this.state.error}</div>
+    ) : (
+      ""
+    );
+
     return (
       <div className="demonym_app">
+        {error}
         <CountrySelector
           countries={this.state.countries}
           changeHandler={(selected) => this.setSelected(selected)}
