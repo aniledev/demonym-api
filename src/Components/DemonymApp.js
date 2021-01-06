16dd2f6ef7b9960cf75b92cc6184af8acbf62b12
aniledev <elinamcgill@gmail.com>
2021-01-05 22:55:59 -0500

    FEAT: initliaze state and use this.setState to update state

--- src/Components/DemonymApp.js
+++ src/Components/DemonymApp.js
@@ -4,6 +4,27 @@ import Demonym from "./Demonym";
 import "../Styles/DemonymApp.css";
 
 export default class DemonymApp extends Component {
+  //initialize state using a constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+      selected: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://country.register.gov.uk/records.json?page-size=5000")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = Object.keys(data).map((key) => data[key].item[0]);
+        console.log(countries);
+        this.setState({
+          countries,
+        });
+      });
+  }
+
   render() {
     return (
       <div className="DemonymApp">
